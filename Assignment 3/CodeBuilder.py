9cb06b8535f89d89d723859012d752f866842d77
Jake <15jps1@queensu.ca>
2019-10-25 14:07:19 -0400

    Bisect insort working

--- Assignment 3/CodeBuilder.py
+++ Assignment 3/CodeBuilder.py
@@ -22,12 +22,7 @@ class FrequencyNode:
 
 # binaryInsert() inserts a node into the minheap structure based on the elements frequency in O(logn) time.
 def binaryInsert(sorted_nodes_list, element):
-    print("New Iteration: ")
-    for node in sorted_nodes_list:
-        print(node.ascii_code)
-        print(node.frequency)
-    if len(sorted_nodes_list) <= 2:
-        print(sorted_nodes_list)
+    if len(sorted_nodes_list) <= 1:
         if sorted_nodes_list[0].frequency >= element.frequency:
             sorted_nodes_list.insert(0, element)
             return sorted_nodes_list
@@ -36,12 +31,11 @@ def binaryInsert(sorted_nodes_list, element):
             return sorted_nodes_list
     else:
         mid_index = math.floor(len(sorted_nodes_list)/2) - 1
-        print("Mid Index: " + str(mid_index))
         if sorted_nodes_list[mid_index].frequency > element.frequency:
-            sorted_nodes_list = binaryInsert(sorted_nodes_list[0:mid_index], element) + sorted_nodes_list[mid_index + 1: len(sorted_nodes_list) - 1]
+            sorted_nodes_list = binaryInsert(sorted_nodes_list[0:mid_index], element) + sorted_nodes_list[mid_index:len(sorted_nodes_list)]
             return sorted_nodes_list
         elif sorted_nodes_list[mid_index].frequency < element.frequency:
-            sorted_nodes_list = sorted_nodes_list[0:mid_index - 1] + binaryInsert(sorted_nodes_list[mid_index: len(sorted_nodes_list) - 1], element)
+            sorted_nodes_list = sorted_nodes_list[0:mid_index+1] + binaryInsert(sorted_nodes_list[mid_index+1:len(sorted_nodes_list)], element)
             return sorted_nodes_list
         else:
             sorted_nodes_list.insert(mid_index, element)
@@ -81,21 +75,6 @@ def defineCodewords(alphabet_frequencies):
     for frequency in sorted_frequencies:
         sorted_frequencies_nodes.append(FrequencyNode(frequency, [None, None]))
 
-    old_list = []
-    for i in range(5):
-        if i != 3:
-            old_list.append(FrequencyNode([32+i, i], [None, None]))
-    
-    for elem in old_list:
-        print(elem.frequency)
-
-    element = FrequencyNode([35, 1], [None, None])
-    print("Old List: " + str(old_list))
-    new_list = binaryInsert(old_list, element)
-
-    for elem in new_list:
-        print(elem.frequency)
-
     # Create a Huffman tree containing all of the frequency nodes.
     while(len(sorted_frequencies_nodes) > 1):
         left_child = sorted_frequencies_nodes.pop(0)
@@ -104,9 +83,9 @@ def defineCodewords(alphabet_frequencies):
         new_ascii = [left_child.ascii_code, right_child.ascii_code]
         new_node = FrequencyNode([new_ascii, new_frequency], [left_child, right_child])
         sorted_frequencies_nodes = binaryInsert(sorted_frequencies_nodes, new_node)
-        for freq in sorted_frequencies_nodes:
-            print(freq.frequency)
-            print(freq.ascii_code)
+
+    print("Root Left Child: " + str(sorted_frequencies_nodes.left_child))
+    print("Root Right Child: " + str(sorted_frequencies_nodes.right_child))
 
     # Initialize the codewords dictionary to "" for all printable characters.
     alphabet_codewords = {'10': ""}
@@ -120,6 +99,6 @@ def main():
     line_list = readZipFile(canonical_collection)
     alphabet_frequencies = extractAlphabetFrequencies(line_list)
     alphabet_codewords = defineCodewords(alphabet_frequencies)
-    print(alphabet_codewords)
+    #print(alphabet_codewords)
 
 main()
